0883eddd15e5b00c4cdf2ccedda2fa282a9fbe1d
Theano Petersen <v-thepet@microsoft.com>
2025-10-03 11:34:59 -0700

    Change timezone from US/Eastern to UTC

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ require __DIR__ . '/vendor/autoload.php';
 use Carbon\Carbon;
 
 
-printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
+printf("Right now in New York is %s.\n", Carbon::now('UTC')->format('Y-m-d g:i:s A'));
 
-printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
+printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('UTC')->format('Y-m-d'));
 
-printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+printf("Right now in Paris is %s\n", Carbon::now('UTC'));
